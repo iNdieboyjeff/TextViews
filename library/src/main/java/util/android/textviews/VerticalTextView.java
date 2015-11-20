c5b54a4d9afcfacfe536b177a19df277547505f7
Jeff Sutton <indieboy1979@gmail.com>
2015-11-20 10:21:04 +0100

    Add VerticalTextView widget

---
+++ library/src/main/java/util/android/textviews/VerticalTextView.java
@@ -0,0 +1,77 @@
+/*
+ *  Copyright (c) 2015 Jeff Sutton
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package util.android.textviews;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.widget.TextView;
+
+/**
+ * @since 1.1.3
+ * @author Jeff Sutton
+ */
+public class VerticalTextView extends TextView {
+
+    final boolean topDown;
+
+    public VerticalTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        final int gravity = getGravity();
+        if (Gravity.isVertical(gravity) && (gravity & Gravity.VERTICAL_GRAVITY_MASK) == Gravity.BOTTOM) {
+            setGravity((gravity & Gravity.HORIZONTAL_GRAVITY_MASK) | Gravity.TOP);
+            topDown = false;
+        } else
+            topDown = true;
+
+    }
+
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(heightMeasureSpec, widthMeasureSpec);
+        setMeasuredDimension(getMeasuredHeight(), getMeasuredWidth());
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        TextPaint textPaint = getPaint();
+        textPaint.setColor(getCurrentTextColor());
+        textPaint.drawableState = getDrawableState();
+
+        canvas.save();
+
+        if (topDown) {
+            canvas.translate(getWidth(), 0);
+            canvas.rotate(90);
+        } else {
+            canvas.translate(0, getHeight());
+            canvas.rotate(-90);
+        }
+
+
+        canvas.translate(getCompoundPaddingLeft(), getExtendedPaddingTop());
+
+        getLayout().draw(canvas);
+        canvas.restore();
+    }
+
+}
